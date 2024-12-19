ee4ab64ad6d87188c54446464897a7470f440958
sagor-ece17 <chakrabortysagor@gmail.com>
2024-12-20 01:42:22 +0600

    model form created

--- task_hive/tasks/views.py
+++ task_hive/tasks/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
+    form = TaskForm()
     tasks = Task.objects.all()
 
-    context = {'tasks' : tasks}
+    context = {'tasks' : tasks, 'TaskForm':form}
     return render(request, 'tasks.html', context)
