025a41b3c8f6b215044fe6395a2c83442905af31
sagor-ece17 <chakrabortysagor@gmail.com>
2024-12-20 01:08:57 +0600

    rendering tasks in our template done

--- task_hive/tasks/views.py
+++ task_hive/tasks/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Task
+
 def index(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.all()
+
+    context = {'tasks' : tasks}
+    return render(request, 'tasks.html', context)
