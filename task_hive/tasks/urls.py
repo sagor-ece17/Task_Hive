e0565ac25828372351c04f779adb79fa37d6696e
sagor-ece17 <chakrabortysagor@gmail.com>
2024-12-20 04:05:13 +0600

    delete feature implemented

--- task_hive/tasks/urls.py
+++ task_hive/tasks/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
      path('',views.index, name=""),
      path('update-task/<str:pk>/', views.updateTask, name="update-task"),
+     path('delete-task/<str:pk>/', views.deleteTask, name="delete-task"),
 ]
--- task_hive/tasks/views.py
+++ task_hive/tasks/views.py
@@ -31,3 +31,15 @@ def updateTask(request,pk):
         return redirect('/')
     context = {'TaskForm':form}
     return render(request, 'update-task.html', context)
+
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+
+        task.delete()
+
+        return redirect('/')
+    context = {'task': task}
+
+    return render(request, "delete-task.html", context)
